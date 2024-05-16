d6aa91d02169ffc1c28075f5d091cc8c1e007f6d
Norbert Penel <n.penel@oscss-shop.fr>
2024-05-16 10:35:41 +0200

    fix table conditions

--- sql/llx_c_conditions.sql
+++ sql/llx_c_conditions.sql
@@ -6,23 +6,30 @@
 CREATE TABLE `llx_c_conditions` (
   `rowid` int(11) NOT NULL,
   `label` varchar(255) NOT NULL,
+  `color` varchar(9) NOT NULL DEFAULT '#000000',
   `position` int(11) NOT NULL DEFAULT 0,
   `active` int(11) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB;
 
 --
 -- Index pour la table `llx_c_conditions`
 --
 ALTER TABLE `llx_c_conditions`
   ADD PRIMARY KEY (`rowid`);
+
+--
+-- AUTO_INCREMENT pour la table `llx_c_conditions`
+--
 ALTER TABLE `llx_c_conditions`
   MODIFY `rowid` int(11) NOT NULL AUTO_INCREMENT;
 --
---  données de la table `llx_c_conditions`
+-- Déchargement des données de la table `llx_c_conditions`
 --
 
-INSERT INTO `llx_c_conditions` (`rowid`, `label`, `position`, `active`) VALUES
-(1, 'BadCondition', 1, 1),
-(2, 'PoorCondition', 2, 1),
-(3, 'GoodCondition', 3, 1),
-(4, 'ExcellentCondition', 4, 1);
+INSERT INTO `llx_c_conditions` (`rowid`, `label`, `color`, `position`, `active`) VALUES
+(1, 'BadCondition', '#FF0000', 1, 1),
+(2, 'PoorCondition', '#FFA500', 2, 1),
+(3, 'GoodCondition', '#BFBF00', 3, 1),
+(4, 'ExcellentCondition', '#00FF00', 4, 1);
+
+
